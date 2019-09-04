35ba967869de2c283cab9265fe05c1fa89324c55
Ubuntu <ubuntu@ip-172-31-29-221.ec2.internal>
2019-09-04 11:37:53 +0000

    this is not my first commit, more like my 9th, but i had to shift all of my code over to a new repository unfortunately my old envoirnment ran into server issues

---
+++ appli.py
@@ -0,0 +1,40 @@
+# this is the root python file 
+# I built a route that sends song data 
+# (fetched from genius) to the page.html templates file
+# to use the data dynamically on the page
+
+from flask import Flask, render_template
+import os
+import requests
+import json
+import random
+
+# app execution name is appli
+appli = Flask(__name__)
+
+# defining authorization header in myheader :)
+myheader = {"Authorization": "Bearer eTnvJMtGEvFKJVrj72lNjVZzXgtyE1xD6Q-Unv2A0Amjhgfx-DEo-1oEUnipH87b"}
+
+song_id=[3300945, 484106, 484106] # multiple songs to randomly generate
+
+# retreiving song api from genius and converting to json
+song = requests.get('https://api.genius.com/songs/' + str(random.choice(song_id)) + '?text_format=plain', headers=myheader)
+song_json = song.json()
+
+# just print(song_string) to inspect json data tree
+song_string = json.dumps(song_json, indent=2) 
+
+# main route, returns .json api data to page.html 
+# uses render_template to access page.html
+@appli.route("/")
+def songdata():
+    return render_template('page.html', 
+    song_art= song_json['response']['song']['song_art_image_url'],
+    song_fulltitle = song_json['response']['song']['full_title'],
+    song_lyrics = song_json['response']['song']['embed_content']
+)
+
+appli.run(host=os.getenv('IP', '0.0.0.0'), 
+          port=int(os.getenv('PORT', 8080)),
+          debug=True)
+
